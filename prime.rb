e38151c2f6ab69a74c969e41b5be52ac6a67c803
eselbeus <eselbeus@gmail.com>
2019-01-08 09:36:56 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ def prime?(int)
   if int < 2 
     return false
   end
-  arr = 2..Math.floor(Math.sqrt(int))
+  arr = 2..Math.sqrt(int)
   for num in arr
     if int % num == 0 
       return false
